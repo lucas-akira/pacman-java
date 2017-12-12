a85f60f11b149344ab19701872ff8615ea156329
TNanukem <tiago.nanu@gmail.com>
2017-12-12 11:08:56 -0200

    BackgroundElement class created

--- src/elements/BackgroundElement.java
+++ src/elements/BackgroundElement.java
@@ -1,30 +1,26 @@
+package elements;
+
+import java.awt.Graphics;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package elements;
-
-import java.awt.Graphics;
-import utils.Drawing;
 
 /**
  *
  * @author tiago
  */
-public class Wall extends BackgroundElement{
+public class BackgroundElement extends Element {
 
-    public Wall(String imageName) {
+    public BackgroundElement(String imageName) {
         super(imageName);
-        this.isTransposable = false;
     }
 
-    
-    
     @Override
     public void autoDraw(Graphics g) {
-        Drawing.draw(g, this.imageIcon, pos.getY(), pos.getX());
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
     
 }
